7761cd4edb4cc46fb2c04f100d37f1f9c8ffbd43
Fred Akers <knivey@botops.net>
2020-06-21 15:47:22 -0400

    Add bing

--- lolbot.php
+++ lolbot.php
@@ -43,6 +43,49 @@ Loop::run( function() {
             return;
         }
 
+        if ($a[0] == '.bing') {
+            if(!isset($a[1])) {
+                $bot->pm($chan, "give me something to lookup");
+                return;
+            }
+
+            \Amp\asyncCall(function () use ($a, $bot, $chan) {
+                global $config;
+                unset($a[0]);
+                $query = urlencode(htmlentities(implode(' ', $a)));
+                $url = $config['bingEP'] . "search?q=$query";
+                try {
+                    $client = HttpClientBuilder::buildDefault();
+                    $request = new Request($url);
+                    $request->setHeader('Ocp-Apim-Subscription-Key', $config['bingKey']);
+                    /** @var Response $response */
+                    $response = yield $client->request($request);
+                    $body = yield $response->getBody()->buffer();
+                    if($response->getStatus() != 200) {
+                        // Just in case its huge or some garbage
+                        $body = substr($body, 0, 200);
+                        $bot->pm($chan, "Error (" . $response->getStatus() . ") $body");
+                        return;
+                    }
+                    $j = json_decode($body, true);
+
+                    if(!array_key_exists('webPages', $j)) {
+                        $bot->pm($chan, "\2Bing:\2 No Results");
+                        return;
+                    }
+                    $results = $j['webPages']['totalEstimatedMatches'];
+                    $res = $j['webPages']['value'][0];
+
+                    $bot->pm($chan, "\2Bing (\2$results Results\2):\2 $res[url] ($res[name]) -- $res[snippet]");
+                } catch (HttpException $error) {
+                    // If something goes wrong Amp will throw the exception where the promise was yielded.
+                    // The HttpClient::request() method itself will never throw directly, but returns a promise.
+                    echo $error;
+                    $bot->pm($chan, "\2WA:\2" . $error);
+                }
+            });
+        }
+
         if ($a[0] == '.stock') {
             if(!isset($a[1])) {
                 $bot->pm($chan, "give me something to lookup");
