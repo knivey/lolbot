b71840ccfd4c85b2764b4129b093ed00915df90b
knivey <knivey@botops.net>
2021-05-13 22:27:57 -0700

    Add special url code for github links

--- scripts/linktitles/linktitles.php
+++ scripts/linktitles/linktitles.php
@@ -30,6 +30,16 @@ function linktitles(\Irc\Client $bot, $chan, $text)
         }
         $link_ratelimit = time() + 2;
 
+        //Handle github user or project
+        if(preg_match("@https?://(?:www\.)?github\.com/([^/]+)(?:/([^/]+))?/?@i", $word, $m)) {
+            $user = $m[1];
+            $repo = $m[2] ?? null;
+            if($out = yield from github($user, $repo)) {
+                $bot->pm($chan, $out);
+                continue;
+            }
+        }
+
         try {
             $client = HttpClientBuilder::buildDefault();
             $req = new Request($word);
@@ -62,5 +72,102 @@ function linktitles(\Irc\Client $bot, $chan, $text)
             echo "$error\n";
             //$bot->pm($chan, "LinkTitles Exception: " . $error);
         }
+    }
+}
+
+function github($user, $repo) {
+    if($repo != null)
+        return yield from github_repo($user, $repo);
+    try {
+        $client = HttpClientBuilder::buildDefault();
+        $req = new Request("https://api.github.com/users/$user");
+        /** @var Response $response */
+        $response = yield $client->request($req);
+        $body = yield $response->getBody()->buffer();
+        if ($response->getStatus() != 200) {
+            echo "Github repos/$user/$repo lookup error: {$response->getStatus()}\n";
+            var_dump($body);
+            return false;
+        }
+        $body = json_decode($body, true);
+        $hireable = "false";
+        if($body['hireable'])
+            $hireable = "true";
+        $created = reftime($body["created_at"]);
+        return "\x02[GitHub]\x02 $body[login] \x02Name:\x02 $body[name] \x02Created:\x02 $created \x02Location:\x02 $body[location] \x02Hireable:\x02 $hireable \x02Public Repos:\x02 $body[public_repos] \x02Followers:\x02 $body[followers] \x02Bio:\x02 $body[bio]";
+    } catch (\Exception $error) {
+        echo "Github repos/$user/$repo exception: $error\n";
+        return false;
+    }
+}
+
+function github_repo($user, $repo) {
+    try {
+        $client = HttpClientBuilder::buildDefault();
+        $req = new Request("https://api.github.com/repos/$user/$repo");
+        /** @var Response $response */
+        $response = yield $client->request($req);
+        $body = yield $response->getBody()->buffer();
+        if ($response->getStatus() != 200) {
+            echo "Github repos/$user/$repo lookup error: {$response->getStatus()}\n";
+            var_dump($body);
+            return false;
+        }
+        $body = json_decode($body, true);
+        $langs = yield from github_langs($user, $repo);
+        $pushed_at = reftime($body["pushed_at"]);
+        $out =  "\x02[GitHub]\x02 $body[full_name] ";
+        if($body['fork']) {
+            $out .= "(Fork of {$body["parent"]["full_name"]}) ";
+        }
+        return "{$out}- $body[description] | $langs | \x02Stargazers:\x02 $body[stargazers_count] \x02Watchers:\x02 $body[watchers_count] \x02Forks:\x02 $body[forks] \x02Open Issues:\x02 $body[open_issues] \x02Last Push:\x02 $pushed_at";
+    } catch (\Exception $error) {
+        echo "Github repos/$user/$repo exception: $error\n";
+        return false;
+    }
+}
+
+function reftime($time) {
+    try {
+        $out = strtotime($time);
+        return strftime("%r %F %Z", $out);
+    } catch (\Exception $e) {
+        return $time;
+    }
+}
+
+function github_langs($user, $repo) {
+    try {
+        $client = HttpClientBuilder::buildDefault();
+        $req = new Request("https://api.github.com/repos/$user/$repo/languages");
+        /** @var Response $response */
+        $response = yield $client->request($req);
+        $body = yield $response->getBody()->buffer();
+        if ($response->getStatus() != 200) {
+            echo "Github repos/$user/$repo/languages lookup error: {$response->getStatus()}\n";
+            var_dump($body);
+            return false;
+        }
+        $body = json_decode($body, true);
+        $total = array_sum($body);
+        $colors = ["12", "08", "09", "13"];
+        $cnt = 0;
+        $langs = [];
+        $percentCnt = 0;
+        foreach ($body as $lang => $lines) {
+            if(!isset($colors[$cnt]))
+                break;
+            $percentCnt += $percent = round($lines / $total) * 100;
+            if($cnt == 3)
+                $langs[] = "\x03". $colors[$cnt] . (100 - $percentCnt) . "% Other";
+            else
+                $langs[] = "\x03". $colors[$cnt] . "{$percent}% $lang";
+            $cnt++;
+        }
+
+        return implode(" ", $langs) . "\x0F";
+    } catch (\Exception $error) {
+        echo "Github repos/$user/$repo/languages exception: $error\n";
+        return false;
     }
 }
