967bc8302757b3c3b4e686eb38bf2c34d0051a39
Fred Akers <knivey@botops.net>
2021-12-02 18:10:37 -0500

    youtube make live stick out more

--- scripts/youtube/youtube.php
+++ scripts/youtube/youtube.php
@@ -37,7 +37,7 @@ function ytDuration($input) {
         }
         $dur = trim($dur);
         if ($dur == '') {
-            $dur = '\x0204⚫ LIVE';
+            $dur = "\x0204⚫ LIVE";
         }
     } catch (\Exception $e) {
         return '???';
