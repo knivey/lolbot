de3246e4ba78dccb3384b43bbbbcc82363f09cea
knivey <knivey@botops.net>
2022-02-01 22:19:34 -1000

    have gcc include std library headers

--- scripts/codesand/common.php
+++ scripts/codesand/common.php
@@ -154,7 +154,37 @@ function runGcc($args, \Irc\Client $bot, \knivey\cmdr\Request $req) {
         return;
     }
     $maxlines = $config['codesand_maxlines'] ?? 10;
-    $output = yield from getRun("/run/gcc?maxlines=$maxlines&flags=-Wno-implicit-function-declaration", $req->args['code']);
+    $code = "#include <assert.h>
+#include <complex.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fenv.h>
+#include <float.h>
+#include <inttypes.h>
+#include <iso646.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdalign.h>
+#include <stdarg.h>
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+#include <string.h>
+#include <tgmath.h>
+#include <threads.h>
+#include <time.h>
+#include <uchar.h>
+#include <wchar.h>
+#include <wctype.h>
+{$req->args['code']}";
+    $output = yield from getRun("/run/gcc?maxlines=$maxlines&flags=-Wno-implicit-function-declaration", $code);
     sendOut($bot, $args->chan, $output);
 }
 
