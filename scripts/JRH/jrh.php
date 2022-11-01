cec9abc9b2a118f73ecd48b34b8039ac8e338bf8
Fred Akers <knivey@botops.net>
2022-11-01 12:45:23 -0400

    update links for new birdnest show domain

--- scripts/JRH/jrh.php
+++ scripts/JRH/jrh.php
@@ -10,8 +10,7 @@ use function scripts\youtube\getLiveVideos;
 #[Cmd("masshl")]
 function masshl($args, \Irc\Client $bot, \knivey\cmdr\Request $req) {
     $jewbirdHosts = [
-        "*@vegan.fried.chicken.with.sodiumchlori.de",
-        "*@*.jewbird.live",
+        "*@vegan.fried.chicken.with.sodiumchlori.de"
     ];
     foreach($jewbirdHosts as $hostmask) {
         if (preg_match(hostmaskToRegex($hostmask), $args->fullhost)) {
@@ -106,24 +105,23 @@ $title
       😁 😆 😅 😂 🤣 ☺️ 😊 😇 🙂 🙃 😉 😌 😍 ASCIIBIRD DEVELOPMENT STREAM 😀 😃 😄 😁 😆 😅 😂 🤣 ☺️ 😊 😇 🙂 🙃
                    WATCH THE WONDERFUL BIRD AND ASCIIBIRD NEARING COMPLETION LIVE NOW
                            ┏   ┰╛    ╔═━┉┈┉╼━━╌┈╍┅┉╌┄┉┉━═╾─┈═──┄┈╼╍═┈┄╍═╍╼━┈─┈╼┉╍┅╌╮
-                         ╘███╏████╒█ ┕█   http://jewbird.live/                     ╏
-                            █┻█  █┦█  █╕  http://yt.jewbird.live/                  ┇
-                          ╔╼█ ████ ████╚━ http://patreon.jewbird.live/             ┃
-                         ╕  █ █ █┉╍█ ┌█═  http://streamlabs.jewbird.live/          ╽
+                         ╘███╏████╒█ ┕█   http://birdnest.live/                    ╏
+                            █┻█  █┦█  █╕  http://yt.birdnest.live/                 ┇
+                          ╔╼█ ████ ████╚━ http://patreon.birdnest.live/            ┃
+                         ╕  █ █ █┉╍█ ┌█═  http://streamlabs.birdnest.live/         ╽
                        ━█████ █ ██ █ ╯█   ASCIIBIRD TAKING FLIGHT ASCIIBIRD FLIGHT ╎
                           ┸╮    ╛     ╘╼┈┅┅──━┈┉┅┈╍┄┈┄┈╍┉╾╾╼╍═━╾╾┄╼╾═─┈═┉═╼┅─┈━╌╾╾┅╯
                               [BTC] 1L2u8mQs5pe7k11ozn2BgX388e3fGMD7qo
 [XMR] 832owKc3ZuGCnmjHXHeZeeJzGAxyKx5uWU9WxoaXg6BhQ7aWSnZ6EhxFK8Mzw137nSgGAfMM8FgHjM6rpq5s1EofD7UT2yp
-           [STREAMLABS] http://streamlabs.jewbird.live [PATREON] http://patreon.jewbird.live
-     [YT] http://yt.jewbird.live [TWITCH] http://twitch.jewbird.live [GITHUB] http://git.jewbird.live
+           [STREAMLABS] http://streamlabs.birdnest.live [PATREON] http://patreon.birdnest.live
+     [YT] http://yt.birdnest.live [TWITCH] http://twitch.birdnest.live [GITHUB] http://git.birdnest.live
 😀 😃 😄 😁 😆 😅 😂 🤣 ☺️ 😊 😇 🙂 🙃 😉 😌 😍 ASCIIBIRD DEVELOPMENT STREAM 😀 😃 😄 😁 😆 😅 😂 🤣 ☺️ 😊 😇 🙂 🙃
 https://www.youtube.com/watch?v={$v->id->videoId}                                https://twitch.tv/birdnestshow
 $title
  NOW LIVE NOW LIVE NOW LIVE NOW LIVE NOW LIVE NOW LIVE NOW LIVE NOW LIVE NOW LIVE NOW LIVE NOW LIVE";
     $banner = trim($banner);
-    $net = strtolower($bot->getOption('NETWORK'));
     if(!isset($config['throttle']) || $config['throttle']) {
-        $bot->msg($args->chan, "Birdnest now live! http://jewbird.live/ {$v->snippet->title}");
+        $bot->msg($args->chan, "Birdnest now live! http://birdnest.live/ {$v->snippet->title}");
         return;
     }
     foreach (explode("\n", $banner) as $line) {
