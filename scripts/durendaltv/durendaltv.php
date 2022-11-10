d35e1cbe491c10b080d5ff2766cc70a79ff1bb61
knivey <knivey@botops.net>
2022-11-10 05:45:36 -0500

    Update durendal's stream to detect if offline and include mpv url

--- scripts/durendaltv/durendaltv.php
+++ scripts/durendaltv/durendaltv.php
@@ -9,12 +9,19 @@ use knivey\cmdr\attributes\Syntax;
 #[CallWrap("Amp\asyncCall")]
 function durendaltv($args, \Irc\Client $bot, \knivey\cmdr\Request $req)
 {
-    global $config;
+    try {
+        yield async_get_contents("https://live.internetrelaychat.net/live/stream.m3u8");
+    } catch (\Exception $e) {
+        $bot->pm($args->chan, "https://live.internetrelaychat.net - stream is offline");
+        return;
+    }
+
     try {
         $data = yield async_get_contents("https://kvdb.io/4gam6KTdmSsvkDFZxFaUHz/now_playing");
-        $bot->pm($args->chan, "https://live.internetrelaychat.net - $data");
+        $bot->pm($args->chan, "https://live.internetrelaychat.net - $data (mpv https://live.internetrelaychat.net/live/stream.m3u8)");
     } catch (\Exception $e) {
         $bot->pm($args->chan, "Error fetching data from https://kvdb.io/4gam6KTdmSsvkDFZxFaUHz/now_playing");
         echo $e->getMessage();
+        $bot->pm($args->chan, "https://live.internetrelaychat.net - stream up but error getting now playing (mpv https://live.internetrelaychat.net/live/stream.m3u8)");
     }
 }
