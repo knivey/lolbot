aa2fd26280800f5edd80528313012178eaa88d28
rj1 <rj1@riseup.net>
2023-06-14 18:50:44 -0600

    fetch tweet information from nitter.net instead of twitter api

--- scripts/twitter/twitter.php
+++ scripts/twitter/twitter.php
@@ -7,10 +7,9 @@ use Amp\Http\Client\Response;
 use Carbon\Carbon;
 use Irc\Exception;
 use scripts\linktitles\UrlEvent;
+use simplehtmldom\HtmlDocument;
 
 global $config;
-if(!isset($config['twitter_bearer']))
-    return; // neat we can do this and it stops the rest of the file from executing
 
 global $eventProvider;
 $eventProvider->addListener(
@@ -25,10 +24,7 @@ $eventProvider->addListener(
             global $config;
             try {
                 $client = HttpClientBuilder::buildDefault();
-                $req = new Request("https://api.twitter.com/2/tweets/$id?tweet.fields=created_at,public_metrics&expansions=author_id");
-                $bearer = $config['twitter_bearer'];
-                $req->addHeader('Authorization', "Bearer $bearer");
-                /** @var Response $response */
+                $req = new Request("https://nitter.net/$user/status/$id");
                 $response = yield $client->request($req);
                 $body = yield $response->getBody()->buffer();
                 if ($response->getStatus() != 200) {
@@ -36,30 +32,18 @@ $eventProvider->addListener(
                     var_dump($body);
                     return;
                 }
-                $j = json_decode($body);
-
-                if(isset($j->errors)) {
-                    if(!is_array($j->errors)) {
-                        echo "twitter errors set but not an array\n";
-                        var_dump($j);
-                        return;
-                    }
-                    $err = $j->errors[0];
-                    $event->reply("[Twitter error] {$err->detail}");
-                    return;
-                }
-
-                $date = Carbon::createFromTimeString($j->data->created_at, 'utc');
+                
+                $html = new HtmlDocument();
+                $html->load($body);
+                $text = $html->find("div.tweet-content", 0)->plaintext;
+                $date = $html->find("p.tweet-published", 0)->plaintext;
+                $date = str_replace("· ", "", $date);
+                $date = Carbon::createFromTimeString($date, 'utc');
                 $ago = $date->shortRelativeToNowDiffForHumans(null, 3);
-                if (is_array($j->includes->users)) {
-                    $user = $j->includes->users[0]->name;
-                    $user = html_entity_decode($user, ENT_QUOTES | ENT_HTML5, 'UTF-8');
-                    $user = str_replace(["\r","\n"], "  ", $user);
-                }
-                $text = $j->data->text;
-                $text = html_entity_decode($text, ENT_QUOTES | ENT_HTML5, 'UTF-8');
-                $text = str_replace(["\r","\n"], "  ", $text);
-                $event->reply("[Twitter] $ago $user tweeted: $text | {$j->data->public_metrics->like_count} likes, {$j->data->public_metrics->reply_count} replies");
+                $like_count = $html->find('div.icon-container span.icon-heart', 0)->parent()->plaintext;
+                $reply_count = $html->find('div.icon-container span.icon-comment', 0)->parent()->plaintext;
+
+                $event->reply("[Twitter] $ago $user tweeted: $text | {$like_count} likes, {$reply_count} replies");
             } catch (Exception $e) {
                 echo "twitter exception {$e->getMessage()}\n";
                 return;
@@ -67,4 +51,3 @@ $eventProvider->addListener(
         });
     }
 );
-
