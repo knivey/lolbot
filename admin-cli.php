0eed013624bd818fd0851accd25cdfc80bdd421d
knivey <knivey@botops.net>
2024-01-18 19:04:57 -0500

    Add doctrine migrations commands to admin-cli.php

--- admin-cli.php
+++ admin-cli.php
@@ -4,12 +4,26 @@ require_once 'bootstrap.php';
 /**
  * @psalm-suppress InvalidGlobal
  */
-global $entityManager;
+global $entityManager, $dependencyFactory;
 
 use lolbot\cli_cmds;
 use Symfony\Component\Console\Application;
+use Doctrine\Migrations\Tools\Console\Command;
 
 $application = new Application();
+
+$application->addCommands(array(
+    new Command\DumpSchemaCommand($dependencyFactory),
+    new Command\ExecuteCommand($dependencyFactory),
+    new Command\GenerateCommand($dependencyFactory),
+    new Command\LatestCommand($dependencyFactory),
+    new Command\ListCommand($dependencyFactory),
+    new Command\MigrateCommand($dependencyFactory),
+    new Command\RollupCommand($dependencyFactory),
+    new Command\StatusCommand($dependencyFactory),
+    new Command\SyncMetadataCommand($dependencyFactory),
+    new Command\VersionCommand($dependencyFactory),
+));
 
 $application->add(new cli_cmds\ignore_add());
 $application->add(new cli_cmds\ignore_del());
