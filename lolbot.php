35b7e4138f3d57de90ed6950994d146f9d3e9e15
AndroidKitKat <me@michaeleisemann.com>
2024-06-09 23:16:24 +0000

    here is my code

--- lolbot.php
+++ lolbot.php
@@ -87,6 +87,7 @@ require_once 'scripts/durendaltv/durendaltv.php';
 require_once 'scripts/bomb_game/bomb_game.php';
 require_once 'scripts/translate/translate.php';
 require_once 'scripts/yoda/yoda.php';
+require_once 'scripts/yoda/yoda-og.php';
 
 
 
