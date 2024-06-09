adb1482fd7eee1e65118bf2862efd67edc5d7ce3
rj1 <rj1@rj1.su>
2024-06-08 20:29:28 -0600

    yoda

--- lolbot.php
+++ lolbot.php
@@ -98,6 +98,7 @@ require_once 'scripts/reddit/reddit.php';
 require_once 'scripts/durendaltv/durendaltv.php';
 require_once 'scripts/bomb_game/bomb_game.php';
 require_once 'scripts/translate/translate.php';
+require_once 'scripts/yoda/yoda.php';
 
 
 $router->loadFuncs();
