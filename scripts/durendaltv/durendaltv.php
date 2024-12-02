e88eb838bac253c00d505e105aa86c2130df0bf5
knivey <knivey@botops.net>
2024-12-01 23:48:29 -0500

    update durendaltv to get title from new kvdb url

--- scripts/durendaltv/durendaltv.php
+++ scripts/durendaltv/durendaltv.php
@@ -16,10 +16,12 @@ function durendaltv($args, \Irc\Client $bot, \knivey\cmdr\Args $cmdArgs)
     }
 
     try {
-        $data = async_get_contents("https://kvdb.io/4gam6KTdmSsvkDFZxFaUHz/now_playing");
+        $kvdb_url = "http://live-kvdb.florp.us";
+        //$data = async_get_contents("https://kvdb.io/4gam6KTdmSsvkDFZxFaUHz/now_playing");
+        $data = async_get_contents($kvdb_url);
         $bot->pm($args->chan, "https://live.internetrelaychat.net - $data (mpv https://live.internetrelaychat.net/live/stream.m3u8)");
     } catch (\Exception $e) {
-        $bot->pm($args->chan, "Error fetching data from https://kvdb.io/4gam6KTdmSsvkDFZxFaUHz/now_playing");
+        $bot->pm($args->chan, "Error fetching data from $kvdb_url");
         echo $e->getMessage();
         $bot->pm($args->chan, "https://live.internetrelaychat.net - stream up but error getting now playing (mpv https://live.internetrelaychat.net/live/stream.m3u8)");
     }
