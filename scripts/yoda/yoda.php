812b9ca1244667769a7f3bbe8b31843af09b19e2
knivey <knivey@botops.net>
2025-03-29 03:27:56 -0400

    update yoda for new filehost

--- scripts/yoda/yoda.php
+++ scripts/yoda/yoda.php
@@ -119,7 +119,7 @@ function hostToFilehole(string $filename): Future
         if(!file_exists($filename))
             throw new \Exception("hostToFilehole called with non existant filename: $filename");
         $client = HttpClientBuilder::buildDefault();
-        $request = new Request("https://filehole.org", "POST");
+        $request = new Request("https://upload.beer", "POST");
         $body = new Form();
         $body->addField('url_len', '5');
         $body->addField('expiry', '86400');
@@ -130,7 +130,7 @@ function hostToFilehole(string $filename): Future
         $response = $client->request($request);
         //var_dump($response);
         if ($response->getStatus() != 200) {
-            throw new \Exception("filehole.org returned {$response->getStatus()}");
+            throw new \Exception("upload.beer returned {$response->getStatus()}");
         }
         $respBody = $response->getBody()->buffer();
         return $respBody;
