0fd0f063f2567a1399583423820e5a6957d851af
knivey <knivey@botops.net>
2026-06-06 07:32:35 -0400

    Add FilterPipeline with named result routing and built-in sources

---
+++ library/draw/FilterPipeline.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace draw;
+
+use Psr\Log\LoggerInterface;
+
+class FilterPipeline
+{
+    private array $results = [];
+    private ?Canvas $lastResult = null;
+    private bool $bgWarned = false;
+
+    public function __construct(
+        Canvas $sourceGraphic,
+        private readonly ?LoggerInterface $logger = null,
+    ) {
+        $this->results['SourceGraphic'] = $sourceGraphic;
+        $this->lastResult = $sourceGraphic;
+    }
+
+    public function getResult(string $name): ?Canvas
+    {
+        if (isset($this->results[$name])) {
+            return $this->results[$name];
+        }
+
+        return match ($name) {
+            'SourceAlpha' => $this->buildSourceAlpha(),
+            'BackgroundImage' => $this->buildEmptyCanvas('BackgroundImage'),
+            'BackgroundAlpha' => $this->buildEmptyCanvas('BackgroundAlpha'),
+            default => null,
+        };
+    }
+
+    public function setResult(string $name, Canvas $canvas): void
+    {
+        $this->results[$name] = $canvas;
+        $this->lastResult = $canvas;
+    }
+
+    public function getLastResult(): Canvas
+    {
+        return $this->lastResult;
+    }
+
+    private function buildSourceAlpha(): Canvas
+    {
+        $source = $this->results['SourceGraphic'];
+        $alpha = Canvas::createBlank($source->w, $source->h, $source->halfblocks);
+
+        for ($y = 0; $y < $source->h; $y++) {
+            for ($x = 0; $x < $source->w; $x++) {
+                $sp = $source->data[$y][$x];
+                $dp = $alpha->data[$y][$x];
+                if ($sp->fg !== null) {
+                    $dp->fg = Color::White;
+                    $dp->fgAlpha = $sp->fgAlpha;
+                }
+                if ($sp->bg !== null) {
+                    $dp->bg = Color::White;
+                    $dp->bgAlpha = $sp->bgAlpha;
+                }
+            }
+        }
+
+        $this->results['SourceAlpha'] = $alpha;
+        return $alpha;
+    }
+
+    private function buildEmptyCanvas(string $sourceName): Canvas
+    {
+        if (!$this->bgWarned) {
+            $this->logger?->warning("SVG filter source '{$sourceName}' is not supported, using empty canvas");
+            $this->bgWarned = true;
+        }
+        $source = $this->results['SourceGraphic'];
+        $empty = Canvas::createBlank($source->w, $source->h, $source->halfblocks);
+        return $empty;
+    }
+}
--- tests/Canvas/FilterPrimitiveTest.php
+++ tests/Canvas/FilterPrimitiveTest.php
@@ -2,7 +2,11 @@
 
 namespace Tests\Canvas;
 
+use draw\Canvas;
+use draw\Color;
+use draw\FilterPipeline;
 use draw\FilterRegion;
+use draw\IrcPalette;
 use PHPUnit\Framework\TestCase;
 
 class FilterPrimitiveTest extends TestCase
@@ -43,5 +47,97 @@ class FilterPrimitiveTest extends TestCase
         $this->assertSame(5.0, $absolute['y']);
         $this->assertSame(20.0, $absolute['w']);
         $this->assertSame(10.0, $absolute['h']);
+    }
+
+    public function test_pipeline_stores_named_result(): void
+    {
+        $source = Canvas::createBlank(10, 10);
+        $source->drawPoint(5, 5, new Color(4, null));
+
+        $pipeline = new FilterPipeline($source);
+        $result = Canvas::createBlank(10, 10);
+        $result->drawPoint(3, 3, new Color(0, null));
+        $pipeline->setResult('blur1', $result);
+
+        $this->assertSame($result, $pipeline->getResult('blur1'));
+    }
+
+    public function test_pipeline_provides_source_graphic(): void
+    {
+        $source = Canvas::createBlank(10, 10);
+        $source->drawPoint(5, 5, new Color(4, null));
+
+        $pipeline = new FilterPipeline($source);
+        $sg = $pipeline->getResult('SourceGraphic');
+
+        $this->assertSame(4, $sg->data[5][5]->fg);
+        $this->assertNull($sg->data[0][0]->fg);
+    }
+
+    public function test_pipeline_provides_source_alpha(): void
+    {
+        $source = Canvas::createBlank(10, 10);
+        $source->drawPoint(5, 5, new Color(4, null));
+
+        $pipeline = new FilterPipeline($source);
+        $sa = $pipeline->getResult('SourceAlpha');
+
+        $pixel = $sa->data[5][5];
+        $this->assertNotNull($pixel->fg);
+        $rgb = IrcPalette::getRgb($pixel->fg);
+        $this->assertSame(255, $rgb[0]);
+        $this->assertSame(255, $rgb[1]);
+        $this->assertSame(255, $rgb[2]);
+    }
+
+    public function test_pipeline_source_alpha_empty_where_source_empty(): void
+    {
+        $source = Canvas::createBlank(10, 10);
+        $source->drawPoint(5, 5, new Color(4, null));
+
+        $pipeline = new FilterPipeline($source);
+        $sa = $pipeline->getResult('SourceAlpha');
+
+        $this->assertNull($sa->data[0][0]->fg);
+    }
+
+    public function test_pipeline_background_image_is_empty(): void
+    {
+        $source = Canvas::createBlank(10, 10);
+        $pipeline = new FilterPipeline($source);
+        $bg = $pipeline->getResult('BackgroundImage');
+
+        $this->assertNull($bg->data[0][0]->fg);
+    }
+
+    public function test_pipeline_background_alpha_is_empty(): void
+    {
+        $source = Canvas::createBlank(10, 10);
+        $pipeline = new FilterPipeline($source);
+        $ba = $pipeline->getResult('BackgroundAlpha');
+
+        $this->assertNull($ba->data[0][0]->fg);
+    }
+
+    public function test_pipeline_returns_null_for_unknown_result(): void
+    {
+        $source = Canvas::createBlank(10, 10);
+        $pipeline = new FilterPipeline($source);
+
+        $this->assertNull($pipeline->getResult('nonexistent'));
+    }
+
+    public function test_pipeline_last_result_tracks_output(): void
+    {
+        $source = Canvas::createBlank(10, 10);
+        $pipeline = new FilterPipeline($source);
+
+        $this->assertSame($source, $pipeline->getLastResult());
+
+        $result = Canvas::createBlank(10, 10);
+        $result->drawPoint(3, 3, new Color(0, null));
+        $pipeline->setResult('step1', $result);
+
+        $this->assertSame($result, $pipeline->getLastResult());
     }
 }
